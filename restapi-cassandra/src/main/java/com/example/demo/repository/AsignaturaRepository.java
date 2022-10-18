80fb31796dc5cf868a17a74e9c7dd695ad2d6ec4
JM-d3l4t0rr3 <jdela@unal.edu.co>
2022-10-17 19:15:05 -0500

    updating

--- restapi-cassandra/src/main/java/com/example/demo/repository/AsignaturaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-import org.springframework.data.repository.CrudRepository;
-import com.example.demo.model.Asignatura;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface AsignaturaRepository extends CrudRepository<Asignatura, Integer> {
-}
--- restapi-cassandra/src/main/java/com/example/demo/repository/ProfesoresRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-import com.example.demo.model.Profesores;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Service;
-
-
-@Service
-public interface ProfesoresRepository extends CrudRepository<Profesores, Integer> {
-}
